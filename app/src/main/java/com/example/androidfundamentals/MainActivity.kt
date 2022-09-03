675e87fa59358389c851c8a658a91e3811e0f3c3
Tushar <bodalwartushar@gmail.com>
2022-09-03 16:19:41 +0530

    Navigation view

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.androidfundamentals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.order.setOnClickListener{
+
+         if (binding.button1.text==null ||binding.button2.text==null||binding.button3.text==null){
+             Toast.makeText(this,"Please select any one option",Toast.LENGTH_SHORT).show()
+         }
+            if (binding.checkBox1.text==null){
+
+
+            }
+
+
+        }
     }
 }
